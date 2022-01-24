17bc633174e88b933b56960600945f2fa96f39ee
Ziyu Chen <46736533+ziyuchen7@users.noreply.github.com>
2022-01-24 11:30:22 -0500

    Update figure_6.m

--- Figures/figure_6.m
+++ Figures/figure_6.m
@@ -1,80 +1,78 @@
 clear all; close all;
+% Robustness test with respect to random translations of peaks
+initstate(1);
+N = 300;
+x = zeros(N,1);
+y = zeros(N,1);
 
-% Code for figure 6
-% Robustness test with respect to gaussian with envelope and additive noise
-initstate(3);
-N = 301;
-x1 = zeros(N,1);
-x2 = zeros(N,1);
-orig_x1 = zeros(N,1);
-orig_x2 = zeros(N,1);
+T = 19;
+D = 1;
+amp = 7;
 
-
-t = 4;
-[ch,Dh,tt] = hermfun(2*t+1,1,8);
-h = ch(1,:);
-
-amp1 = 3;
-amp2 = 3;
-T1 = 27;
-T2 = 17;
-D = 0.2;
-for i = T1:T1:N
-    orig_x1(i-t:i+t) = amp1.*h';
+for i = T:T:N
+    j = i;
     d = rand;
-    d = 2*D*d-D;
-    x1(i-t:i+t) = orig_x1(i-t:i+t).*(1+d);
+    d = round(2*D*d-D);
+    j = j + d;
+    x(j) = amp;
 end
 
-t = 6;
-[ch,Dh,tt] = hermfun(2*t+1,1,12);
-h = ch(1,:);
-
-for i = T2:T2:N
-    orig_x2(i-t:i+t) = amp2.*h';
-    d = rand;
-    d = 2*D*d-D;
-    x2(i-t:i+t) = orig_x2(i-t:i+t).*(1+d);
+for i = T:T:N
+    y(i) = amp;
 end
 
-x = x1 + x2;
-sigma = 2;
-t = (1:N)/N;
-env = exp(-t.^2./sigma^2)';
+% T2 = 7;
+% for i = T2:T2:N
+%     j = i;
+%     d = rand;
+%     d = round(2*D*d-D);
+%     j = j + d;
+%     x(j) = x(j) + 7;
+% end
+% 
+% for i = T2:T2:N
+%     y(i) = y(i) + 10;
+% end
 
-x = x.*env;
-orig_x = orig_x1 + orig_x2;
-SNR = 5;
-ns = randn(length(x),1);%*10^(-1*SNR*norm(x)/20);
-ns = ns/norm(ns)*norm(x)*10^(-SNR/20);
-x = x + ns;
+%s = Regularized_Strength_vs_Period_L1_figure(x,100,'Ramanujan',0.001);
+%s = Normalized_Regularized_Strength_vs_Period_L1_figure(x,100,'Ramanujan',0.1);
+s = PD_Lasso(x,50,'Ramanujan',1,2);
 
+%% small to large
+[s1] = small2large(x,0.5,50,2);
+%% M-best
+[s2] = mbest(x,1,50,2);
+%% best correlation
+[s3] = bestcorrelation(x,50,1,2);
 
-s = PD_Lasso(x,50,'Ramanujan',0.2,2);
+%%
 k = 15;
-SNR = 20*log10(std(orig_x)/std(ns))
-figure
+figure()
 subplot(421)
-plot(orig_x1,'color',[0 0 0]);
-xlabel('time');axis tight;set(gca, 'fontsize', k);
+plot(y,'color','k');axis tight;
+xlabel('time');set(gca, 'fontsize', k);
 subplot(422)
-plot(x1,'color',[0 0 0]);
-xlabel('time');axis tight;set(gca, 'fontsize', k);
+plot(x,'color','k');axis tight;
+xlabel('time');set(gca, 'fontsize', k);
 subplot(423)
-plot(orig_x2,'color',[0 0 0]);
-xlabel('time');axis tight;set(gca, 'fontsize', k);
+plot(x,'color','r');axis tight;
+hold on 
+plot(y,'color','k');axis tight;
+xlabel('time');
+set(gca, 'fontsize', k);
 subplot(424)
-plot(x2,'color',[0 0 0]);
-xlabel('time');axis tight;set(gca, 'fontsize', k);
-subplot(425)
-plot(x1+x2,'color',[0 0 0]);
-xlabel('time');axis tight;set(gca, 'fontsize', k);
-subplot(426)
-plot(env,'color',[0 0 0])
-xlabel('time');axis tight;set(gca, 'fontsize', k);
-subplot(427)
-plot(x,'color',[0 0 0]);
-xlabel('time');axis tight;set(gca, 'fontsize', k);
-subplot(428)
 stem(s,'linewidth',3,'color',[0 0 0]);
-xlabel('period');xlim([1 50]);set(gca, 'fontsize', k);
+xlabel('Period');axis tight;
+set(gca, 'fontsize', k);
+subplot(425)
+stem(s1,'linewidth',3,'color',[0 0 0]);
+xlabel('period');axis tight;
+set(gca, 'fontsize', k);
+subplot(426)
+stem(s2,'linewidth',3,'color',[0 0 0]);
+xlabel('period');axis tight;
+set(gca, 'fontsize', k);
+subplot(4,2,7.5)
+stem(s3,'linewidth',3,'color',[0 0 0]);
+xlabel('period');axis tight;
+set(gca, 'fontsize', k);
