d2af43896ab6ab9ab194fdd55ffad80cfef20724
Ziyu Chen <46736533+ziyuchen7@users.noreply.github.com>
2022-01-24 11:30:06 -0500

    Update figure_5.m

--- Figures/figure_5.m
+++ Figures/figure_5.m
@@ -38,36 +38,60 @@ x = x.*env;
 
 s = PD_Lasso(x,50,'Ramanujan',0.8,1);
 k = 15;
+
+%% small to large
+[s1] = small2large(x,0.3,50,1);
+
+%% M-best
+[s2] = mbest(x,2,50,1);
+
+%% best correlation
+[s3] = bestcorrelation(x,50,2,1);
+
+
+%%
 figure
-subplot(421)
+subplot(621)
 stem(orig_x1,'linewidth',3,'color',[0 0 0]);
 xlabel('time');axis tight;
 set(gca, 'fontsize', k);
-subplot(422)
+subplot(622)
 stem(x1,'linewidth',3,'color',[0 0 0]);
 xlabel('time');axis tight;
 set(gca, 'fontsize', k);
-subplot(423)
+subplot(623)
 stem(orig_x2,'linewidth',3,'color',[0 0 0]);
 xlabel('time');axis tight;
 set(gca, 'fontsize', k);
-subplot(424)
+subplot(624)
 stem(x2,'linewidth',3,'color',[0 0 0]);
 xlabel('time');axis tight;
 set(gca, 'fontsize', k);
-subplot(425)
+subplot(625)
 stem(x1+x2,'linewidth',3,'color',[0 0 0]);
 xlabel('time');axis tight;
 set(gca, 'fontsize', k);
-subplot(426)
+subplot(626)
 plot(env,'color',[0 0 0])
 xlabel('time');axis tight;
 set(gca, 'fontsize', k);
-subplot(427)
+subplot(627)
 stem(x,'linewidth',3,'color',[0 0 0]);
 xlabel('time');axis tight;
 set(gca, 'fontsize', k);
-subplot(428)
+subplot(628)
 stem(s,'linewidth',3,'color',[0 0 0]);
+xlabel('period');axis tight;
+set(gca, 'fontsize', k);
+subplot(629)
+stem(s1,'linewidth',3,'color',[0 0 0]);
+xlabel('period');axis tight;
+set(gca, 'fontsize', k);
+subplot(6,2,10)
+stem(s2,'linewidth',3,'color',[0 0 0]);
+xlabel('period');axis tight;
+set(gca, 'fontsize', k);
+subplot(6,2,11.5)
+stem(s3,'linewidth',3,'color',[0 0 0]);
 xlabel('period');axis tight;
 set(gca, 'fontsize', k);
